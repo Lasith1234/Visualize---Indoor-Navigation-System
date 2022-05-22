bae594bb4badd67cedd0434aa5b72417ff43a442
lasith <lasithraja@gmail.com>
2022-05-22 16:14:02 +0530

    Creating WAV file

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,12 @@
 #include "AudioFileSourcePROGMEM.h"
 #include "AudioGeneratorWAV.h"
 #include "AudioOutputI2SNoDAC.h"
+#include <iostream>
+#include <cmath>
+#include <fstream>
+
+using namespace std;
+
 
 //WiFI===========================================================
 
@@ -15,6 +21,78 @@ const char* password = "";
 
 #define motorInput 14
 
+//WAV File Meta Data=============================================
+
+const int sampleRate = 44100;
+const int bitDepth = 16;
+
+//===============================================================
+
+class sineOscillator{
+  float frequency, amplitude, angle = 0.0f ,offset = 0.0;
+
+  public:
+    sineOscillator(float freq, float amp) : frequency(freq), amplitude(amp) {
+      offset = 2 * PI * frequency / sampleRate;
+    }
+    float process(){
+      auto sample = amplitude * sin(angle);
+      angle += offset;
+    
+      return sample;
+    }
+
+};
+
+void writeToFile(ofstream &file, int value, int size){
+  file.write(reinterpret_cast<const char*> (&value), size);
+}
+
+int createWAV(){
+  int duration =2;
+  ofstream audioFile;
+  audioFile.open("waveform.wav", ios::binary);
+  sineOscillator soc(440, 0.5);
+
+  //Header chunk
+  audioFile << "RIFF";
+  audioFile << "----";
+  audioFile << "WAVE";
+
+  //Format chunk
+  audioFile << "fmt ";
+  writeToFile(audioFile, 16, 4);  //Size
+  writeToFile(audioFile, 1, 2);   //compression code
+  writeToFile(audioFile, 1, 2);   //Number of channels
+  writeToFile(audioFile, sampleRate, 4);    //Sample Rate
+  writeToFile(audioFile, sampleRate * bitDepth / 8, 4);  //Byte Rate
+  writeToFile(audioFile, bitDepth / 8, 2);   //Block Align
+  writeToFile(audioFile, bitDepth, 2); //Bit Depth
+
+  //Data Chunk
+  audioFile << "data";
+  audioFile << "----";
+
+  int preAudioPosition = audioFile.tellp();
+  auto maxAmplitude = pow(2, bitDepth - 1) - 1;
+  for (int i = 0; i < sampleRate * duration; i++)
+  {
+    auto sample = soc.process();
+    int intSample = static_cast<int> (sample * maxAmplitude);
+    writeToFile(audioFile, intSample, 2);
+  }
+
+  int postAudioPosition = audioFile.tellp();
+
+  audioFile.seekp(preAudioPosition - 4);
+  writeToFile(audioFile, postAudioPosition - preAudioPosition, 4);
+
+  audioFile.seekp(4, ios::beg);
+  writeToFile(audioFile, postAudioPosition - 8, 4);
+
+  audioFile.close();
+  return 0;  
+}
 
 void wifiConnect(){
   Serial.println("Connecting");
@@ -107,9 +185,6 @@ void playSpeaker(){
       break;
     }
   }
-  
-
-
    
 }
 
